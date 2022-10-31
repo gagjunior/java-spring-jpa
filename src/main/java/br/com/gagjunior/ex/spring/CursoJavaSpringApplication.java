36c213ce01f395c7b72a51e045cbc9657166dce4
Gilberto Junior <gagjunior@hotmail.com>
2022-10-30 23:29:56 -0300

    first commit

---
+++ src/main/java/br/com/gagjunior/ex/spring/CursoJavaSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.gagjunior.ex.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoJavaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoJavaSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gagjunior/ex/spring/CursoJavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gagjunior.ex.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoJavaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
