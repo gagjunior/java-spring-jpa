d4e5c68ec424bbb7505d748a61e711c9424eb8a6
Gilberto Junior <gagjunior@hotmail.com>
2022-11-11 00:07:34 -0300

    New class 'Payment'

--- src/main/java/br/com/gagjunior/ex/spring/entities/Order.java
+++ src/main/java/br/com/gagjunior/ex/spring/entities/Order.java
@@ -1,114 +1,110 @@
 package br.com.gagjunior.ex.spring.entities;
 
+import br.com.gagjunior.ex.spring.entities.enums.OrderStatus;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import javax.persistence.*;
 import java.io.Serializable;
 import java.time.Instant;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
-
-import com.fasterxml.jackson.annotation.JsonFormat;
-
-import br.com.gagjunior.ex.spring.entities.enums.OrderStatus;
-
 @Entity
 @Table(name = "tb_order")
 public class Order implements Serializable {
 
     private static final long serialVersionUID = 1L;
-
+    @OneToMany(mappedBy = "id.order")
+    private final Set<OrderItem> items = new HashSet<>();
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
-
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
-
     private Integer orderStatus;
-
-    @OneToMany(mappedBy = "id.order")
-    private Set<OrderItem> items = new HashSet<>();
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    private Payment payment;
 
     public Order() {
 
     }
 
     public Order(Long id, Instant moment, User client, OrderStatus orderStatus) {
-	this.id = id;
-	this.moment = moment;
-	this.client = client;
-	setOrderStatus(orderStatus);
+        this.id = id;
+        this.moment = moment;
+        this.client = client;
+        setOrderStatus(orderStatus);
     }
 
     public Long getId() {
-	return id;
+        return id;
     }
 
     public void setId(Long id) {
-	this.id = id;
+        this.id = id;
     }
 
     public Instant getMoment() {
-	return moment;
+        return moment;
     }
 
     public void setMoment(Instant moment) {
-	this.moment = moment;
+        this.moment = moment;
     }
 
     public User getClient() {
-	return client;
+        return client;
     }
 
     public void setClient(User client) {
-	this.client = client;
+        this.client = client;
     }
 
     public OrderStatus getOrderStatus() {
-	try {
-	    return OrderStatus.valueOf(orderStatus);
-	} catch (IllegalAccessException e) {
-	    throw new IllegalArgumentException(e.getMessage());
-	}
+        try {
+            return OrderStatus.valueOf(orderStatus);
+        } catch (IllegalAccessException e) {
+            throw new IllegalArgumentException(e.getMessage());
+        }
     }
 
     public void setOrderStatus(OrderStatus orderStatus) {
-	if (orderStatus != null) {
-	    this.orderStatus = orderStatus.getCode();
-	}
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     public Set<OrderItem> getItems() {
-	return items;
+        return items;
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
     }
 
     @Override
     public int hashCode() {
-	return Objects.hash(id);
+        return Objects.hash(id);
     }
 
     @Override
     public boolean equals(Object obj) {
-	if (this == obj)
-	    return true;
-	if (obj == null)
-	    return false;
-	if (getClass() != obj.getClass())
-	    return false;
-	Order other = (Order) obj;
-	return Objects.equals(id, other.id);
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Order other = (Order) obj;
+        return Objects.equals(id, other.id);
     }
 
 }
