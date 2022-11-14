57b49251231a30c8e6b0f874fe9b6582a1235ae3
Gilberto Junior <gagjunior@hotmail.com>
2022-11-13 23:06:50 -0300

    User: delete version 1

--- src/main/java/br/com/gagjunior/ex/spring/controllers/UserController.java
+++ src/main/java/br/com/gagjunior/ex/spring/controllers/UserController.java
@@ -38,4 +38,9 @@ public class UserController {
         return ResponseEntity.created(uri).body(newUser);
     }
 
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        userService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
