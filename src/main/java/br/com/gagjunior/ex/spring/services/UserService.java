b94f0cc5f697467e9294c286c8b4a5238b37bb3a
Gilberto Junior <gagjunior@hotmail.com>
2022-11-13 23:26:59 -0300

    Class User: update

--- src/main/java/br/com/gagjunior/ex/spring/services/UserService.java
+++ src/main/java/br/com/gagjunior/ex/spring/services/UserService.java
@@ -28,7 +28,19 @@ public class UserService {
         return userRepository.save(user);
     }
 
-    public void delete(Long id){
+    public void delete(Long id) {
         userRepository.deleteById(id);
+    }
+
+    public User update(Long id, User user) {
+        User entity = userRepository.getReferenceById(id);
+        updateData(entity, user);
+        return userRepository.save(entity);
+    }
+
+    private void updateData(User entity, User user) {
+        entity.setName(user.getName());
+        entity.setEmail(user.getEmail());
+        entity.setPhone(user.getPhone());
     }
 }
